985c1eba94357bc9bc6cee3082958f482e7b8e5f
RomeoA23 <efimovleonid@ukr.net>
2018-10-02 15:55:18 +0300

    Added BMI1 support

--- src/types.h
+++ src/types.h
@@ -78,6 +78,10 @@
 #  include <nmmintrin.h> // Intel and Microsoft header for _mm_popcnt_u64()
 #endif
 
+#if defined(USE_BMI1) && (defined(__INTEL_COMPILER) || defined(_MSC_VER))
+#  include <immintrin.h>
+#endif
+
 #if !defined(NO_PREFETCH) && (defined(__INTEL_COMPILER) || defined(_MSC_VER))
 #  include <xmmintrin.h> // Intel and Microsoft header for _mm_prefetch()
 #endif
@@ -99,6 +103,12 @@
 #define HasPext 1
 #else
 #define HasPext 0
+#endif
+
+#ifdef USE_BMI1
+#define USE_BMI1 1
+#else
+#define USE_BMI1 0
 #endif
 
 #ifdef IS_64BIT
